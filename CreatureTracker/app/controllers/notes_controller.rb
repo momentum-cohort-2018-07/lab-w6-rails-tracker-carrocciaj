82a3272a955e13b6e6a7575feda034a057aa9633
Josh Carroccia <agr.carroccia@gmail.com>
2018-09-05 17:47:15 -0400

    MVP requirements done

--- CreatureTracker/app/controllers/notes_controller.rb
+++ CreatureTracker/app/controllers/notes_controller.rb
@@ -2,14 +2,14 @@ class NotesController < ApplicationController
   before_action :set_note, only: [:show, :update, :destroy]
   
   def show
-    @user = User.find(params[:user_id])
-    render json: creature_path(@creature)
+    @creature = Creature.find(params[:creature_id])
+    render json: @note
   end
   
   def create
-  @creature = Creature.find(params[:user_id])
+  @creature = Creature.find(params[:creature_id])
   @note = @creature.notes.create(note_params)
-  render json: creature_path(@creature)
+  render json: @note
 end
 
 def update
